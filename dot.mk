4d3e7ba50b3a92cae0e4dac7ad3bbd9411bcdfaa
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-10-07 01:12:36 -0400

    on7ltechn: Add dotOS Makefile
    Change-Id: I64c87c86113d8fc9ca16858d9ff47394f93e3e06

---
+++ dot.mk
@@ -0,0 +1,14 @@
+# Inherit from common
+$(call inherit-product, device/samsung/o7-common/dot.mk)
+
+$(call inherit-product, device/samsung/on7ltechn/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := on7ltechn
+PRODUCT_NAME := dot_on7ltechn
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G6000
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
